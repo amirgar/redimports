52a69b303e08bf28644f739fb35637611727105d
amirgar <garam08@bk.ru>
2026-01-23 20:55:05 +0300

    Updated settings.py to allow Amvera configuration

--- backend/catalog/static/catalog/js/script.js
+++ backend/catalog/static/catalog/js/script.js
@@ -1,12 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const tg = window.Telegram.WebApp;
-    tg.ready();
     tg.expand();
-
-    // Отключаем закрытие свайпом вниз, чтобы работал внутренний скролл
-    if (tg.isVersionAtLeast('7.7')) {
-        tg.disableVerticalSwipes();
-    }
-
-    tg.setHeaderColor('#FFFFFF');
+    tg.setHeaderColor('#ffffff');
+    tg.setBackgroundColor('#ffffff');
+    tg.ready();
 });
