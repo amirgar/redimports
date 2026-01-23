3e79c15b38184498b430ab602f53896172ffe06b
amirgar <garam08@bk.ru>
2026-01-23 21:04:27 +0300

    Updated settings.py to allow Amvera configuration

--- backend/catalog/static/catalog/js/script.js
+++ backend/catalog/static/catalog/js/script.js
@@ -1,3 +1,9 @@
+const tg = window.Telegram.WebApp;
+tg.expand(); 
+tg.setHeaderColor('#ffffff'); 
+tg.setBackgroundColor('#ffffff');
+tg.ready();
+
 window.addEventListener('DOMContentLoaded', () => {
     const tg = window.Telegram.WebApp;
     tg.expand();
